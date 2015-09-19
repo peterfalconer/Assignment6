a331582b0d5c2c39bebb5700de673f175faf70ff
peterfalconer <p.falconer@acfonline.org.au>
2015-09-19 16:13:39 +1000

    Added alternative method using addClass and removeClass

--- js/index.js
+++ js/index.js
@@ -1,8 +1,12 @@
 $(document).ready(function() {
 
       var cities = ["NEWYORK","NEW YORK CITY","NY","NYC","SAN FRANCISCO","SF","BAY AREA","LOS ANGELES","LA","LAX","SYDNEY","SYD","AUSTIN","ATX"];
-      var images = ["nyc.jpg","nyc.jpg","nyc.jpg","nyc.jpg","sf.jpg","sf.jpg","sf.jpg","la.jpg", "la.jpg", "la.jpg", "sydney.jpg", "sydney.jpg","austin.jpg","austin.jpg"];
+      var images = ["nyc.jpg","nyc.jpg","nyc.jpg","nyc.jpg","sf.jpg","sf.jpg","sf.jpg","la.jpg", "la.jpg", "la.jpg", "sydney.jpg", "sydney.jpg","austin.jpg","austin.jpg"];      
       var selections = ["NYC","SF","LA","ATX","SYD"]
+      // The following variables are used for the alternative method - using addClass and removeClass function. To use, initialise the useClassMethod var to true
+      var useClassMethod = false;
+      var classes = ["nyc","nyc","nyc","nyc","sf","sf","sf","la", "la", "la", "sydney", "sydney","austin","austin"]
+      var currentClass = ""
 
       $("#city-type").on('change', UpdateCityImage);
       fillSelector();
@@ -35,9 +39,19 @@ $(document).ready(function() {
       function showCityAsBackground(cityName) {
         var i = 0;
         var bgURL = ""
+
         i = cities.indexOf(cityName.toUpperCase().replace(/ /g, ""));
-        bgURL = "url(images/" + images[i] + ")"
-        $("body").css("background-image", bgURL);
+        if (useClassMethod) {
+          // Alternative method - using addClass and removeClass
+          if (currentClass !== "") {
+            $("body").removeClass(currentClass);
+          }
+          currentClass =  classes[i]
+          $("body").addClass(currentClass);
+        } else {
+          bgURL = "url(images/" + images[i] + ")"
+          $("body").css("background-image", bgURL);
+        }
       }
 
       function showDefaultCityBackground(cityName) {
