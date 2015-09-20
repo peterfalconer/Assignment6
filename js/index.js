40798091b0a0c65f719bd1d91b5a06ae75512b5b
peterfalconer <p.falconer@acfonline.org.au>
2015-09-20 17:59:16 +1000

    After linting

--- js/index.js
+++ js/index.js
@@ -1,60 +1,61 @@
-$(document).ready(function() {
+$(document).ready(function () {
 
-      var cities = ["NEWYORK","NEW YORK CITY","NY","NYC","SAN FRANCISCO","SF","BAY AREA","LOS ANGELES","LA","LAX","SYDNEY","SYD","AUSTIN","ATX"];
-      var images = ["nyc.jpg","nyc.jpg","nyc.jpg","nyc.jpg","sf.jpg","sf.jpg","sf.jpg","la.jpg", "la.jpg", "la.jpg", "sydney.jpg", "sydney.jpg","austin.jpg","austin.jpg"];      
-      var selections = ["NYC","SF","LA","ATX","SYD"]
-      // The following variables are used for the alternative method - using addClass and removeClass function. To use, initialise the useClassMethod var to true
-      var useClassMethod = false;
-      var classes = ["nyc","nyc","nyc","nyc","sf","sf","sf","la", "la", "la", "sydney", "sydney","austin","austin"]
-      var currentClass = ""
+  var cities = ["NEWYORK", "NEW YORK CITY", "NY", "NYC", "SAN FRANCISCO", "SF", "BAY AREA", "LOS ANGELES", "LA", "LAX", "SYDNEY", "SYD", "AUSTIN", "ATX"];
+  var images = ["nyc.jpg", "nyc.jpg", "nyc.jpg", "nyc.jpg", "sf.jpg", "sf.jpg", "sf.jpg", "la.jpg", "la.jpg", "la.jpg", "sydney.jpg", "sydney.jpg", "austin.jpg", "austin.jpg"];
+  var selections = ["NYC", "SF", "LA", "ATX", "SYD"];
+  // The following variables are used for the alternative method - using addClass and removeClass function. To use, initialise the useClassMethod var to true
+  var useClassMethod = false;
+  var classes = ["nyc", "nyc", "nyc", "nyc", "sf", "sf", "sf", "la", "la", "la", "sydney", "sydney", "austin", "austin"];
+  var currentClass = "";
+  var i = 0;
 
-      $("#city-type").on('change', UpdateCityImage);
-      fillSelector();
+  function fillSelector() {
+    for (i = selections.length - 1; i >= 0; i--) {
+      $("#city-type").append("<option>" + selections[i] + "</option>");
+    }
+  }
 
-      function UpdateCityImage(e) {
-        var cityName = "";
-        e.preventDefault();
-        cityName = $("#city-type").val();
-        if (citynameIsValid(cityName)) {
-            showCityAsBackground(cityName);
-        } else {
-            showDefaultCityBackground(cityName);
-        }
+  function citynameIsValid(cityName) {
+    var result = false;
+    if (cities.indexOf(cityName.toUpperCase().replace(/ /g, "")) > -1) {
+      result = true;
+    }
+    return result;
+  }
+
+  function showCityAsBackground(cityName) {
+    var j = 0;
+    var bgURL = "";
+
+    j = cities.indexOf(cityName.toUpperCase().replace(/ /g, ""));
+    if (useClassMethod) {
+      // Alternative method - using addClass and removeClass
+      if (currentClass !== "") {
+        $("body").removeClass(currentClass);
       }
-      
-      function fillSelector() {
-        for (var i = selections.length - 1; i >= 0; i--) {
-          $("#city-type").append("<option>" + selections[i] + "</option>");
-        };        
-      }
+      currentClass =  classes[j];
+      $("body").addClass(currentClass);
+    } else {
+      bgURL = "url(images/" + images[j] + ")";
+      $("body").css("background-image", bgURL);
+    }
+  }
 
-      function citynameIsValid(cityName) {
-        var result = false;
-        if (cities.indexOf(cityName.toUpperCase().replace(/ /g, "")) > -1) {
-            result = true;
-        }
-        return result;
-      }
+  function showDefaultCityBackground() {
+    $("body").css("background-image", "url(images/citipix_skyline.jpg)");
+  }
 
-      function showCityAsBackground(cityName) {
-        var i = 0;
-        var bgURL = ""
+  function UpdateCityImage(e) {
+    var cityName = "";
+    e.preventDefault();
+    cityName = $("#city-type").val();
+    if (citynameIsValid(cityName)) {
+      showCityAsBackground(cityName);
+    } else {
+      showDefaultCityBackground(cityName);
+    }
+  }
 
-        i = cities.indexOf(cityName.toUpperCase().replace(/ /g, ""));
-        if (useClassMethod) {
-          // Alternative method - using addClass and removeClass
-          if (currentClass !== "") {
-            $("body").removeClass(currentClass);
-          }
-          currentClass =  classes[i]
-          $("body").addClass(currentClass);
-        } else {
-          bgURL = "url(images/" + images[i] + ")"
-          $("body").css("background-image", bgURL);
-        }
-      }
-
-      function showDefaultCityBackground(cityName) {
-        $("body").css("background-image","url(images/citipix_skyline.jpg)");  
-      }
+  $("#city-type").on('change', UpdateCityImage);
+  fillSelector();
 });
